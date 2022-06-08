480b429cb7103b9127103e04787716053205dc43
drshapeless <drsl@drshapeless.com>
2022-06-09 00:34:17 +0800

    Add post handlers.

--- internal/rest/posts.go
+++ internal/rest/posts.go
@@ -1,11 +1,146 @@
 package rest
 
 import (
-	"html/template"
+	"errors"
 	"net/http"
 
 	"github.com/drshapeless/shapeless-blog/internal/data"
+	"github.com/go-chi/chi"
 )
 
+func (app *Application) showPostHandler(w http.ResponseWriter, r *http.Request) {
+	t := chi.URLParam(r, "title")
+	p, err := app.Models.Posts.GetWithURL(t)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	err = app.writeJSONInterface(w, http.StatusOK, p, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
+
 func (app *Application) createPostHandler(w http.ResponseWriter, r *http.Request) {
+	var input struct {
+		Title    string `json:"title"`
+		Content  string `json:"content"`
+		URL      string `json:"url"`
+		CreateAt string `json:"create_at"`
+		UpdateAt string `json:"update_at"`
+	}
+
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	p := &data.Post{
+		Title:    input.Title,
+		Content:  input.Content,
+		URL:      input.URL,
+		CreateAt: input.CreateAt,
+		UpdateAt: input.UpdateAt,
+	}
+
+	err = app.Models.Posts.Insert(p)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	err = app.writeJSONInterface(w, http.StatusCreated, p, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
+
+func (app *Application) updatePostHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+	if err != nil {
+		app.notFoundResponse(w, r)
+		return
+	}
+
+	var input struct {
+		Title    *string `json:"title"`
+		Content  *string `json:"content"`
+		URL      *string `json:"url"`
+		CreateAt *string `json:"create_at"`
+		UpdateAt *string `json:"update_at"`
+	}
+
+	err = app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	var p data.Post
+
+	p.ID = int(id)
+
+	if input.Title != nil {
+		p.Title = *input.Title
+	}
+
+	if input.Content != nil {
+		p.Content = *input.Content
+	}
+
+	if input.URL != nil {
+		p.URL = *input.URL
+	}
+
+	if input.CreateAt != nil {
+		p.CreateAt = *input.CreateAt
+	}
+
+	if input.UpdateAt != nil {
+		p.UpdateAt = *input.UpdateAt
+	}
+
+	err = app.Models.Posts.Update(&p)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrEditConflict):
+			app.editConflictResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	err = app.writeJSONInterface(w, http.StatusOK, p, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
+
+func (app *Application) deletePostHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+	if err != nil {
+		app.notFoundResponse(w, r)
+		return
+	}
+
+	err = app.Models.Posts.Delete(int(id))
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
 }
